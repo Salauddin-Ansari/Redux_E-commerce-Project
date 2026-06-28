e9e02fbf9e6eab06514b1918ea267353a3638f2a
Salauddin Ansari <salauddinansari98356@gmail.com>
2026-06-28 13:20:08 +0530

    fix blank page on vercel

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
+// import { createRoot } from "react-dom/client";
+// import App from "./App";
+// import { Provider } from "react-redux";
+// import { store } from "./store";
+// import { RouterProvider, createBrowserRouter } from "react-router-dom";
+// import Home from "./pages/Home";
+// import Cart from "./pages/Cart";
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     children: [
+//       {
+//         path: "/",
+//         element: <Home />,
+//       },
+//       {
+//         path: "/cart",
+//         element: <Cart />,
+//       },
+//     ],
+//   },
+// ]);
+
+// createRoot(document.querySelector("#root")).render(
+//   <Provider store={store}>
+//     <RouterProvider router={router} />
+//   </Provider>,
+// );
+
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
@@ -11,19 +42,13 @@ const router = createBrowserRouter([
     path: "/",
     element: <App />,
     children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/cart",
-        element: <Cart />,
-      },
+      { index: true, element: <Home /> },
+      { path: "cart", element: <Cart /> },
     ],
   },
 ]);
 
-createRoot(document.querySelector("#root")).render(
+createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <RouterProvider router={router} />
   </Provider>,
